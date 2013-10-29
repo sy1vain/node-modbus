53511dcac7e2dba5df651bd60a1e499b0aed778b
Berk Demirkır <berk.demirkir@ries.com.tr>
2013-10-29 15:22:04 +0200

    Timeout and NoDelay support added. Reading bits is done.

--- test/tcp/test.js
+++ test/tcp/test.js
@@ -16,45 +16,47 @@ describe('ModbusAddress', function() {
 });
 */
 
+var debug = require('debug')('test');
 var modbus = require('../../lib');
 
 // Tries to keep a connection alive after first request
 var client = modbus.tcp.connect({
   port: '502',
   host: 'localhost',
-  responseTimeout: 1000, // net.Socket.setTimeout
+  responseTimeout: 400, // net.Socket.setTimeout
   noDelay: true // net.Socket.setNoDelay
 });
 
 setInterval(function() {
   client.request({
     unit: 1, // Slave ID
-    func: modbus.Functions.READ_COILS, // MODBUS function code
+    func: modbus.Functions.READ_HOLDING_REGISTERS, // MODBUS function code
     address: 0, // 0-65535
-    count: 16,
+    count: 10,
     //type: modbustcp.DataTypes.Int16,
     response: function(err, res) {
       if (err) {
         if (err instanceof modbus.Errors.ArgumentError) {
           // Invalid arguments
-          console.log(err);
+          debug('', err);
         }
         if (err instanceof modbus.Errors.ConnectionError) {
           // Can't establish a connection
-          console.log(err);
+          debug('', err);
         }
-        /*if (err instanceof modbus.Errors.TimeoutError) {
+        if (err instanceof modbus.Errors.TimeoutError) {
           // Communication timed out
-        }*/
+          debug('', err);
+        }
         if (err instanceof modbus.Errors.RequestError) {
           // Exception returned from slave
-          console.log(err);
+          debug('', err);
         }
         // retry or skip
         return;
       }
       // do something
-      console.log(res);
+      debug('', res);
     }
-  });  
+  });
 }, 500);
